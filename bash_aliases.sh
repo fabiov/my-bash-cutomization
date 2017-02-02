2945b71e301b0891f2353f0a37ccbd570f9ad51d
Fabio Ventura <foravatenubi@gmail.com>
2017-02-02 22:25:16 +0100

    add some git alias

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+alias grep='grep --color=auto'
 alias l="ls -hl --color --group-directories-first"
 alias la="ls -ahl --color --group-directories-first"
 alias ll="ls -hl --color --group-directories-first"
