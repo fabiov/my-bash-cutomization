97236a66aa01dbcb426772f911e90390c6ccb034
Fabio <foravatenubi@gmail.com>
2017-03-06 09:41:03 +0100

    Some more alias to avoid making mistakes

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -4,3 +4,8 @@ alias grep='grep --color=auto'
 alias l="ls -hl --color --group-directories-first"
 alias la="ls -ahl --color --group-directories-first"
 alias ll="ls -hl --color --group-directories-first"
+
+# Some more alias to avoid making mistakes:
+alias rm="rm -i"
+alias cp="cp -i"
+alias mv="mv -i"
