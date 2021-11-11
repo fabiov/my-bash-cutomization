649a69678681253fe1a3298ae9e9b0990ad40790
Fabio <foravatenubi@gmail.com>
2021-11-11 20:40:16 +0100

    Update bash_aliases.sh

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-alias grep='grep --color=auto'
+alias dc="docker-compose"
+alias grep="grep --color=auto"
 alias l="ls -hl --color --group-directories-first"
 alias la="ls -ahl --color --group-directories-first"
 alias ll="ls -hl --color --group-directories-first"
